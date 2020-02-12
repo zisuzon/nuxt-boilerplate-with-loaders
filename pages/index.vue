db501ec1994508cc32ffc75e1f76d1d1f1792bf2
ZIS <zisuzon@deligram.com>
2020-02-12 13:09:13 +0600

    added sass

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        {{ saySomething }}
+      </h1>
+      <h2 class="subtitle">
+        My kickass Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  computed: {
+    saySomething () {
+      return 'Yo maaaaan'
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
